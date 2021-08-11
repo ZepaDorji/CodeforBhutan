e701752f04f138d6739edcde19baf5b8f733d170
Zepa Dorji <zepadorji222@gmail.com>
2021-08-11 15:15:22 +0600

    added questions and pattern

--- Questions/patterns.py
+++ Questions/patterns.py
@@ -1,23 +1,34 @@
-# x pattern
+# # x pattern
 
-def xpattern(row):
-    for i in range(row):
-        for j in range(row):
-            if i == j or i + j == row-1:
-                print("*",end = " ")
-            else:
-                print(" ", end= " ")
-        print()
-xpattern(5)
+# def xpattern(row):
+#     for i in range(row):
+#         for j in range(row):
+#             if i == j or i + j == row-1:
+#                 print("*",end = " ")
+#             else:
+#                 print(" ", end= " ")
+#         print()
+# xpattern(5)
 
 
-# quiz question
-b = 1
-for a in range(1,10,3):
-    b+= a + 1
-print(b)
+# # quiz question
+# b = 1
+# for a in range(1,10,3):
+#     b+= a + 1
+# print(b)
 
-x = 50 
-y = 10 
-z = y if y > x else x 
-print(z)
+# x = 50 
+# y = 10 
+# z = y if y > x else x 
+# print(z)
+
+
+n = 6
+for i in range(n):
+    for j in range(n):
+        if i == 0 or j == (n-1) or i == j:
+            print("*", end = "")
+        else:
+            print(end = " ")
+    print()
+        
--- Questions/quiz.py
+++ Questions/quiz.py
@@ -1,43 +1,50 @@
-# try to answers the following questions withous running the programm
+# try to answers the following questions without running the programm
 # Q1
-b = 1
-for a in range(1,10,3):
-    b+= a + 1
-print(b)
+# b = 1
+# for a in range(1,10,3):
+#     b+= a + 1
+# print(b)
 
-#Q2
-x = 50 
-y = 10 
-z = y if y > x else x 
-print(z)
+# #Q2
+# x = 50 
+# y = 10 
+# z = y if y > x else x 
+# print(z)
 
-#Q3
-x = y = z = 300
-print(x,y,z)
+# #Q3
+# x = y = z = 300
+# print(x,y,z)
 
-#Q4
-i = 0
-while i < 5:
-    print(i)
-    i += 1
-    if i == 3:
-        break
+# #Q4
+# i = 0
+# while i < 5:
+#     print(i)
+#     i += 1
+#     if i == 3:
+#         break
 
-#Q5
-print("python" '3')
+# #Q5
+# print("python" '3')
 
-#Q6
-x = "Python is Greate"
-y = x.upper().lower().upper()
-print(y)
+# #Q6
+# x = "Python is Greate"
+# y = x.upper().lower().upper()
+# print(y)
 
-#Q7
-def length(a,b):
-    return a(b)
-print(length(len,"python"))
+# #Q7
+# def length(a,b):
+#     return a(b)
+# print(length(len,"python"))
 
 
-#Q8
-x = ['1','2','3']
-y = x[1] + x[2]
-print(y)
+# #Q8
+# x = ['1','2','3']
+# y = x[1] + x[2]
+# print(y)
+
+#9
+list = [1,2,3,4,5,10]
+print(list.pop(-3), end = " ")
+print(list)
+print(list.remove(list[2]))
+print(list)
